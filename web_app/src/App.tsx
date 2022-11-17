d7e94c558d1c8787103ef1acb505f416c45c9d4c
Christian Knutson <Cknutson575@gmail.com>
2022-11-16 19:50:33 -0800

    Created the mobile app and web app file structure

---
+++ web_app/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { View, StyleSheet } from 'react-native';
+import { Counter } from './shared/components/counter';
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+})
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Counter/>
+    </View>
+  );
+}
+
+export default App;

54a48032ae45abc33e4d1146f4b74d752355a564
Christian Knutson <Cknutson575@gmail.com>
2022-11-23 15:23:36 -0800

    Removed a dependency from App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,13 +6,16 @@ const styles = StyleSheet.create({
   container: {
     width: "100%",
     height: "100%",
+    backgroundColor: "yellow",
   },
 })
 
 function App() {
   return (
     <View style={styles.container}>
-      <Counter/>
+      <View style={styles.container}>
+        <Counter/>
+      </View>
     </View>
   );
 }

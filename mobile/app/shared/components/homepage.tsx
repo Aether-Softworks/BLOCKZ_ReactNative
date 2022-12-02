f1a1f1d06bf7fb39c61f998e4853638dab64175f
Christian Knutson <Cknutson575@gmail.com>
2022-12-01 21:41:22 -0800

    Added Animations to the homepage component

--- mobile/app/shared/components/homepage.tsx
+++ mobile/app/shared/components/homepage.tsx
@@ -1,9 +1,8 @@
-import React, {Component, useState} from 'react';
-import { Switch, StyleSheet, Text, View, ViewStyle, Button, Alert } from 'react-native';
+import React, {Component, useState, useRef, useEffect} from 'react';
+import { Switch, StyleSheet, Text, View, ViewStyle, Button, Alert, Animated } from 'react-native';
 import { Blockzlogo } from './blockzlogo';
 import TableRender from './tableRender';
 import TaskRender from './taskRender';
-import MyView from '../utils/MyView';
 
 
 // Variable to center various views
@@ -33,6 +32,16 @@ const styles = StyleSheet.create({
         height: 148,
         borderRadius: 16,
     },
+    containerSVG: {
+        ...centered,
+        flexDirection: "row",
+        paddingTop: 16,
+        paddingBottom: 16,
+        paddingLeft: 32,
+        paddingRight: 32,
+        justifyContent:"center",
+        alignItems: 'center',
+    },
     titleText: {
         color: '#FFFFFF',
         fontWeight: 'bold',
@@ -40,6 +49,7 @@ const styles = StyleSheet.create({
         padding: 8
     },
     letterText: {
+        ...centered,
         color: '#FFFFFF',
         fontWeight: "normal",
         fontSize: FONT_SIZE_18,
@@ -50,12 +60,76 @@ const styles = StyleSheet.create({
         width: "75%",
         borderRadius: 6,
         overflow: "hidden", 
-    }
+    },
 });
 
 export const Homepage = () => {
-    const [value, setValue] = useState(true)
-  return (
+    const timeblockPos = useRef(new Animated.Value(175)).current
+    const taskPos = useRef(new Animated.Value(-250)).current
+    const opacityTimeBlock = useRef(new Animated.Value(1)).current
+    const opacityTask = useRef(new Animated.Value(0)).current
+    const [latch, setLatch] = useState(true)
+    const [getStarted, setGetStarted] = useState(true)
+
+  
+  const TranslateIn = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(timeblockPos, {
+        useNativeDriver: true,
+        toValue: 175,
+      duration: 500
+    }).start();
+    Animated.timing(opacityTimeBlock, {
+        useNativeDriver: true,
+        toValue: 1,
+        duration: 250
+    }).start();
+    Animated.timing(taskPos, {
+        useNativeDriver: true,
+        toValue: -250,
+        duration: 500,
+    }).start()
+    Animated.timing(opacityTask, {
+        useNativeDriver: true,
+        toValue: 0,
+        duration: 100,
+    }).start()
+  };
+
+  const TranslateOut = () => {
+    Animated.timing(timeblockPos, {
+        useNativeDriver: true,
+        toValue: 275,
+        duration: 500
+    }).start();
+    Animated.timing(opacityTimeBlock, {
+        useNativeDriver: true,
+        toValue: 0,
+        duration: 100
+    }).start();
+    Animated.timing(taskPos, {
+        useNativeDriver: true,
+        toValue: -85,
+        duration: 500,
+    }).start()
+    Animated.timing(opacityTask, {
+        useNativeDriver: true,
+        toValue: 1,
+        duration: 250,
+    }).start()
+  };
+
+    // do {
+    //     for(let i = 0; i < 5000; i++) {
+    //         if(i < 5000) {
+    //             continue
+    //         } else {
+    //             setLatch(!latch)
+    //         }
+    //     }
+    // } while(getStarted == true)
+
+    return (
     <View style={styles.container}>
         <View style={styles.containerBlockz}>
             <View style={{paddingTop: 8}}>
@@ -63,30 +137,25 @@ export const Homepage = () => {
             </View>
             <Text style={styles.titleText}>BLOCKZ</Text>         
         </View>
-        <View style={{padding: 32}}>
-            <View style={{justifyContent:"center", alignItems:"center"}}>
-                <Switch
-        //   onValueChange={value => setValue(!value))}
-        //   value={value}
-        />
-        {/* CHRISTIAN THIS IS WHERE YOU WERE WORKING */}
-                <MyView>
+        <View style={{flexDirection:'column', justifyContent: 'center', alignItems: 'center'}}>
+            <View style={styles.containerSVG}>
+                <Animated.View style={{transform: [{translateX: timeblockPos}], opacity: opacityTimeBlock}}>
                     <TableRender/>
-                </MyView>
-                <Text style={styles.letterText}>BUILD A TIMEBLOCK TABLE!</Text>
-                <MyView>
+                </Animated.View>
+                <Animated.View style={{transform: [{translateX: taskPos}], opacity: opacityTask}}>
                     <TaskRender/>
-                </MyView>
-                <Text style={styles.letterText}>OR A TASK LIST!</Text>
+                </Animated.View>
             </View>
+            <Text style={styles.letterText}>The Budget Builder For Your Time!</Text>
         </View>
         <View style={styles.button}>
             <Button
-                onPress={() => Alert.alert("This will take you to the login/signup screen")}
+                // onPress={() => Alert.alert("This will take you to the login/signup screen")}
+                onPress={() => setGetStarted(false)}
                 title="Get Started"
                 color={BLOCKZ_GREEN}
                 accessibilityLabel="Learn more about this button."
-            />  
+            />
         </View>
     </View>
   );
